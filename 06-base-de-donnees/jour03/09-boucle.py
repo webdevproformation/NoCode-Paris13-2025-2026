dd8a6a5d2cf8f8c2c9edc5aef2086f4d7f428066
Malik H <malik.h@webdevpro.net>
2026-02-12 12:21:06 +0100

    boucle

---
+++ 06-base-de-donnees/jour03/09-boucle.py
@@ -0,0 +1,35 @@
+# boucle en javascript 
+# for (  let i = 0 ; i < 5 ; i++   ){ 
+#
+# }
+
+for i in range(5):
+    pass 
+
+i = 30 
+
+for i in range(10): # range(0, 10 , 1)
+    print(i)
+
+for i in range(2 ,10):
+    print(i) 
+
+for i in range(2 ,10 , 3):
+    print(i) 
+
+# parcourir chaque valeur de mon tableau
+
+fleurs = ["jasmin" , "tulipe", "rose", "lilas"]
+
+for i in fleurs:
+    print(i)
+
+
+# créer le fichier 10-exo.py
+
+# en utilisant une boucle (et concaténation), afficher dans la console les string suivants : 
+# 4 x 2 = 8
+# 4 x 3 = 12
+# 4 x 4 = 16
+# 4 x 5 = 20
+# 4 x 6 = 24
---
+++ 06-base-de-donnees/jour03/10-exo.py
@@ -0,0 +1,9 @@
+# 4 x 2 = 8
+# 4 x 3 = 12
+# 4 x 4 = 16
+# 4 x 5 = 20
+# 4 x 6 = 24
+
+for i in range( 2 , 7 ):
+    calcul = f"4 x {i} = {i * 4}"
+    print(calcul)
